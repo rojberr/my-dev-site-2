303987b808a463b4754e10ae70ce8ea6adb06dc5
rojberr <rojberr@outlook.com>
2022-07-16 18:42:05 +0200

    web(index): Add working contact form

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -17,10 +17,10 @@
       let action = thisForm.getAttribute('action');
       let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
       
-      if( ! action ) {
-        displayError(thisForm, 'The form action property is not set!')
-        return;
-      }
+      // if( ! action ) {
+      //   displayError(thisForm, 'The form action property is not set!')
+      //   return;
+      // }
       thisForm.querySelector('.loading').classList.add('d-block');
       thisForm.querySelector('.error-message').classList.remove('d-block');
       thisForm.querySelector('.sent-message').classList.remove('d-block');
--- forms/contact.php
+++ forms/contact.php
@@ -23,15 +23,13 @@
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
+    'host' => 'smtp.gmail.com',
+    'username' => 'speedysalesmanproject@gmail.com',
     'password' => 'pass',
     'port' => '587'
   );
-  */
+
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
